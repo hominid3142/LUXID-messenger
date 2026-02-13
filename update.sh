f9640bf9488961a2a7c77ce52cc28b5c88c9cb6e
Molut <hominid3142@gmail.com>
2026-02-13 15:29:20 +0900

    Fix docker-compose command

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ git pull
 echo ">> [2/3] Rebuilding and restarting containers..."
 # --build ensures any changes to Dockerfile or requirements are picked up
 # -d runs in detached mode (background)
-docker-compose up -d --build
+docker compose up -d --build
 
 echo ">> [3/3] Pruning unused images (optional cleanup)..."
 docker image prune -f
